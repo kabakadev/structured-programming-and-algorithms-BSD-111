1e13dbfc3a8254ffe5d686716ad4950fd6873f4a
Ian Kabaka <iankabaka1@gmail.com>
2026-05-17 17:17:54 +0300

    Add C programs and gitignore for executables

---
+++ input_program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+    float height;
+    double bank_balance;
+    long phone_number;
+
+    scanf("%f", &height);
+    scanf("%lf", &bank_balance);
+    scanf("%ld", &phone_number);
+
+    printf("Height: %f\n", height);
+    printf("Bank balance amount: %f\n", bank_balance);
+    printf("phone number: %ld\n", phone_number);
+    
+    
+
+}
---
+++ units_offered.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* AUthor: Ian Kabaka, Date May 14 2026 */
+
+int main() {
+    printf("units offered this semester\n");
+    printf("BCU 111: COMMUNICATION SKILLS\n");
+    printf("PHY 111: INTRODUCTION TO PHYSICS\n");
+    printf("BCU 113: HEALTH AND WELLNESS MANAGEMENT\n");
+    printf("MAT 111: BASIC MATHEMATICS\n");
+    printf("BSD 111: Structured programming and algorithms\n");
+    printf("BCE 111: COMPUTATION LOGIC\n");
+    printf("BCU 112: DIGITAL  LITERACY\n");
+    printf("%d\n", 42);
+    return 0;
+    
+
+}
