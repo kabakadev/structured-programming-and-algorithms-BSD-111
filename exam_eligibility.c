4945c3c11e5cac318df26e18bd4faab74eccd1ac
Ian Kabaka <iankabaka1@gmail.com>
2026-05-25 13:43:45 +0300

    eligibility criteria

---
+++ exam_eligibility.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int attendance;
+    float average_marks;
+
+    printf("Enter the class attendance:\n ");
+    scanf("%d", &attendance);
+
+    printf("Enter the average marks: \n");
+    scanf("%f", &average_marks);
+
+    if (attendance >= 75 && average_marks >= 40 ){
+        printf("you are eligible for the exams\n");
+    }
+    else{
+        printf("Not eligible");
+    }
+}
