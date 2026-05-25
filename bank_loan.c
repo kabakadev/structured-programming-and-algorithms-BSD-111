98be67ee80982923c886c22339d43c546f71724a
Ian Kabaka <iankabaka1@gmail.com>
2026-05-25 13:28:03 +0300

    bank loan program logic tested

--- bank_loan.c
+++ bank_loan.c
@@ -4,17 +4,17 @@ int main(){
     int customer_age;
     double customer_income;
 
-    printf("What is your age? ");
-    scanf("%d\n", &customer_age);
+    printf("What is your age?:\n");
+    scanf("%d", &customer_age);
 
-    printf("What is your annual income?");
-    scanf("%lf\n", &customer_income);
+    printf("What is your annual income?:\n");
+    scanf("%lf", &customer_income);
 
     if (customer_age >= 21 && customer_income >= 21000){
-        printf("Congrats you do qualify for a loan");
+        printf("Congrats you do qualify for a loan\n");
     }
     else{
-        printf("Unfortunately we are unable to offer you a loan at this time");
+        printf("Unfortunately we are unable to offer you a loan at this time\n");
     }
     return 0;
 }
