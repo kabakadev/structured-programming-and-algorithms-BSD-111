d00c47c02c833e8af7da91c756a66ff712fbd2ba
Ian Kabaka <iankabaka1@gmail.com>
2026-06-15 00:08:55 +0300

    fix infinite loop when using int

--- week_4/prompt_password.c
+++ week_4/prompt_password.c
@@ -5,22 +5,27 @@
   
 */
 #include <stdio.h>
+#include <string.h> // Required for strcmp
 
 int main(){
 
-    int userinput;
-
+    // 4 characters + 1 for the null terminator '\0'
+    char userinput[5]; 
     
     printf("Enter the correct password to log in, it is a four digit password: ");
     do{
-        scanf("%d",&userinput);
+        // %4s limits input to 4 chars to prevent buffer overflow
+        scanf("%4s", userinput); 
 
-        if (userinput != 1234){
-            printf("you entered, %d\n", userinput);
+        // strcmp returns 0 if the strings match perfectly
+        if (strcmp(userinput, "1234") != 0){
+            printf("you entered, %s\n", userinput);
             printf("Wrong password keep trying: ");
-        } else{
+        } else {
             printf("you got the password!!\n");
         }
-    }while(userinput != 1234);
+        
+    } while(strcmp(userinput, "1234") != 0); // Loop continues as long as they DON'T match
+
     return 0;
 }
