afa3504a6cf7b6ef1280f6002d3d967520d1e02d
Ian Kabaka <iankabaka1@gmail.com>
2026-06-22 00:32:01 +0300

    electric bill exercise done

---
+++ week_5/electric_bill.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int calculateElectricBill(int number_of_units) {
+    int total_bill = 0;
+    
+      if (number_of_units > 200){
+            total_bill = (100 * 10) + (100 * 15) + ((number_of_units - 200) * 20);
+        }  else if (number_of_units <= 200 && number_of_units > 100){
+            total_bill = (100 * 10) + ((number_of_units - 100) * 15);
+        } else{
+            total_bill = number_of_units * 10;
+        }
+
+    printf("The total bill is: %d \n", total_bill);
+    
+    return total_bill;
+}
+
+int main(){
+    int number_of_units = 0;
+
+    printf("enter the number of units consumed: ");
+    scanf("%d", &number_of_units);
+    calculateElectricBill(number_of_units);
+
+    
+      
+
+    return 0;
+
+}
+
