0d032cb541f40a5abc8abce513a4e4fe1adbd36a
Ian Kabaka <iankabaka1@gmail.com>
2026-06-28 23:53:48 +0300

    still working

--- week_6/two_d_array_hotel.c
+++ week_6/two_d_array_hotel.c
@@ -8,11 +8,12 @@
 */
 
 int main(){
+    int a;
     int i;
     int j;
     
     int occupancy[5][10]={1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1};
-    
+    int chain[3][5][10] = {1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1};
     for (i = 0; i<5;i++){
         int vacant = 0;
         int occupied = 0;
@@ -26,6 +27,25 @@ int main(){
         printf("the total vacant for floor %d is %d\n", i,vacant);
         printf("the total occupied for floor %d is %d\n", i,occupied);
     }
-    return 0;
+    
+    int branch = 0;
+    for (int o = 0; o<3; o++){
+        branch++;
+        for (i = 0; i<5;i++){
+            int vacant = 0;
+            int occupied = 0;
+           for (j=0; j<10;j++){
+              if (chain[o][i][j] == 0){
+                  vacant++;
+              }else {
+                  occupied ++;
+            }
+        }
+        printf("the total vacant for floor %d is %d in branch %d\n", i,vacant,branch);
+        printf("the total occupied for floor %d is %d in branch %d\n", i,occupied,branch);
+    }
+    
+}
+return 0;
 
 }
